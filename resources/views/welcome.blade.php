2d28695eeacdfadbef831e1f16fea4eee75402ad
aarathypvinu <aarathypvinu@gmail.com>
2021-03-17 11:53:50 +0530

    student details

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
+</div>
+</div>
+
+<div class="row">
+<div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
+<table class="table table-borderless">
+<tr>
+   <td>name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>roll no</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>admission no.</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>college name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td></td>
+    <td><button class="btn btn-success">Submit</button></td>
+</tr>
+</table>
+</div>
+</div>
+<div class="row">
+<div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
+</div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
