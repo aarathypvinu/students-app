a717b1a95e5282fa210c25300909638e3ce4d369
aarathypvinu <aarathypvinu@gmail.com>
2021-03-17 12:00:43 +0530

    yyy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,11 @@
 </head>
 <body>
 <div class="container">
+
+
 <div class="row">
 <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
 </div>
-</div>
-
-<div class="row">
 <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
 <table class="table table-borderless">
 <tr>
@@ -40,9 +39,9 @@
 </table>
 </div>
 </div>
-<div class="row">
+
 <div class="col col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2">
-</div>
+
 </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
